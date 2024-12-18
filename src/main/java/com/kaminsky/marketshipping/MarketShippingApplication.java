a9511ea5339b947f999c8f651308cb37473c1ade
Philip Kaminsky <kaminsky8836@gmail.com>
2024-12-18 13:36:15 +0300

    initial commit

---
+++ src/main/java/com/kaminsky/marketshipping/MarketShippingApplication.java
@@ -0,0 +1,13 @@
+package com.kaminsky.marketshipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketShippingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketShippingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaminsky/marketshipping/MarketShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminsky.marketshipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketShippingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
